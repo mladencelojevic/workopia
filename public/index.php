adee39f33969649e00f527dafce0f13593c57be1
Mladen Celojevic <mladencmm@gmail.com>
2025-10-13 15:03:22 +0200

    make Router class for basic routing

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 require_once "../helpers.php";
 
-$uri = $_SERVER['REQUEST_URI'];
 
-require_once basePath('router.php'); // contains array of routes and condition
+require_once basePath('Router.php'); // contains array of routes and condition
+$router = new Router();
+$routes = require_once basePath('routes.php');
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD']; // GET OR POST
+
+
+$router->route($uri, $method);
--- router.php
+++ router.php
@@ -1,11 +1,64 @@
 <?php
 
-$routes = require_once basePath('routes.php'); // contains array of routes
+declare(strict_types=1);
+
+class Router
+{
+
+    protected array $routes = [];
 
 
-if (array_key_exists($uri, $routes)) {
-    require_once basePath($routes[$uri]);
-} else {
-    http_response_code(404); // necessary, because dev tool will not make the page as an error. 
-    require_once basePath($routes['404']);
+    public function registerRoute($method, $uri, $controller)
+    {
+        // routes[] = [] - this adds another element to the existing array. routes = [] - this just overwrittes.
+        $this->routes[] = [
+
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller
+
+
+
+        ];
+    }
+
+
+
+
+    public function get(string $uri, string $controller)
+    {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+    public function post(string $uri, string $controller)
+    {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+    public function put(string $uri, string $controller)
+    {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+    public function delete(string $uri, string $controller)
+    {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+
+    public function route($uri, $method)
+    {
+
+        foreach ($this->routes as $route) {
+
+            if ($route['uri'] === $uri && $route['method'] === $method) {
+
+
+                require_once basePath($route['controller']);
+                return;
+            }
+        }
+
+
+        http_response_code(404);
+        loadView('error/404');
+        exit;
+    }
 }
